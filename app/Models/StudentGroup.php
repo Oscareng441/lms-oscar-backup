8b6ee2b74b0d98f9bb59fa0a315bf0a640c01b73
jfarmer <jfarmer@feisrt.com>
2025-09-21 07:36:18 -0600

    20250921d

--- app/Models/StudentGroup.php
+++ app/Models/StudentGroup.php
@@ -12,6 +12,7 @@ class StudentGroup extends Model
     protected $fillable = [
         'id',
         'name',
+        'active',
         'owner_id',
         'course_id',
     ];
@@ -99,7 +100,7 @@ class StudentGroup extends Model
     public static function getAll()
     {
         $sql = '
-        SELECT G.name, S.name as school, C.name as course, G.id, owners, member_count
+        SELECT G.name, S.name as school, C.name as course, G.id, owners, member_count, G.active
         FROM student_groups G
         INNER JOIN courses C ON C.id = G.course_id
         LEFT JOIN school S ON S.id = G.school_id
