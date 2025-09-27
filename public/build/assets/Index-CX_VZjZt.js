b9a3676023d0ac0336bccdda57e1f4e247d1c015
jfarmer <jfarmer@feisrt.com>
2025-09-27 09:45:59 -0600

    20250927b

--- public/build/assets/Index-CX_VZjZt.js
+++
@@ -1,1 +0,0 @@
-import{j as r,B as s}from"./app-B0tcN9qk.js";import{A as o}from"./AuthenticatedLayout-D7caZSIn.js";import"./ApplicationLogo-WoE5Obr7.js";import"./transition-BqabgTyj.js";const p=({auth:t,data:a})=>{const e="Problems";return r.jsx(o,{auth:t,user:t.user,header:e,children:r.jsx(s,{title:e})})};export{p as default};
--- public/build/assets/index-C0QMtVAI.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./app-B0tcN9qk.js";var a=r();export{a as r};
