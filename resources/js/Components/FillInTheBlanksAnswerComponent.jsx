e426defe3659c38ed90b5965cd6597c48f92f85e
jfarmer <jfarmer@feisrt.com>
2026-02-08 10:03:04 -0600

    fix fill in the blanks display wrt wrap

--- resources/js/Components/FillInTheBlanksAnswerComponent.jsx
+++ resources/js/Components/FillInTheBlanksAnswerComponent.jsx
@@ -1,21 +1,23 @@
-import { useState, useEffect } from 'react';
-import AnswerChoice from '@/Components/AnswerChoice';
-import InputLabel from '@/Components/InputLabel';
-import InputError from '@/Components/InputError';
-import { handleFraction } from '@/Helpers/Utilities';
+import { useState, useEffect } from "react";
+import AnswerChoice from "@/Components/AnswerChoice";
+import InputLabel from "@/Components/InputLabel";
+import InputError from "@/Components/InputError";
+import { handleFraction } from "@/Helpers/Utilities";
 
 export default function FillInTheBlanksAnswerComponent(props) {
-    const [selectedAnswers, setSelectedAnswers] = useState(props.selectedAnswers)
-    const [hasAnswered, setHasAnswered] = useState( false )
-    const [slot, setSlot] = useState( 1 )
+    const [selectedAnswers, setSelectedAnswers] = useState(
+        props.selectedAnswers,
+    );
+    const [hasAnswered, setHasAnswered] = useState(false);
+    const [slot, setSlot] = useState(1);
 
     function selectAnswer(ans) {
-        let a = [ ...props.selectedAnswers ]
-        ans.slot_number = slot
-        setSlot(slot + 1)
-        a.push(ans)
-        setSelectedAnswers(a) 
-        props.setSelectedAnswers(a) 
+        let a = [...props.selectedAnswers];
+        ans.slot_number = slot;
+        setSlot(slot + 1);
+        a.push(ans);
+        setSelectedAnswers(a);
+        props.setSelectedAnswers(a);
     }
 
     return (
@@ -26,30 +28,43 @@ export default function FillInTheBlanksAnswerComponent(props) {
                         Haz clic en una opción para llenar la siguiente caja.
                     </div>
                     <div className="flex justify-center flex-wrap">
-                    { props.answers.map((r, k) => {
-                        let isSelected = false
-                        props.selectedAnswers.some(a => {
-                            if (r.answer_text === a.answer_text) {
-                                isSelected = true
-                                return true
-                            }
-                            return false
-                        })
-                        return isSelected ? '' : (
-                            <AnswerChoice key={ k } answer={ r } select={ selectAnswer } selected={ isSelected } selectable={ !hasAnswered } />
-                        )
-                    })}
+                        {props.answers.map((r, k) => {
+                            let isSelected = false;
+                            props.selectedAnswers.some((a) => {
+                                if (r.answer_text === a.answer_text) {
+                                    isSelected = true;
+                                    return true;
+                                }
+                                return false;
+                            });
+                            return isSelected ? (
+                                ""
+                            ) : (
+                                <AnswerChoice
+                                    key={k}
+                                    answer={r}
+                                    select={selectAnswer}
+                                    selected={isSelected}
+                                    selectable={!hasAnswered}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
             </div>
-            {
-                !hasAnswered &&
-                <div className="mx-auto w-48 text-center my-6 space-y-6 sm:px-6 lg:px-8 cursor-pointer" onClick={ () => { props.answerSelect(selectedAnswers); setHasAnswered(true)}}>
+            {!hasAnswered && (
+                <div
+                    className="mx-auto w-48 text-center my-6 space-y-6 sm:px-6 lg:px-8 cursor-pointer"
+                    onClick={() => {
+                        props.answerSelect(selectedAnswers);
+                        setHasAnswered(true);
+                    }}
+                >
                     <div className="bg-white p-6 shadow sm:rounded-lg sm:p-4">
                         Enviar
                     </div>
                 </div>
-            }
+            )}
         </>
     );
 }
--- resources/js/Components/FillInTheBlanksBox.jsx
+++ resources/js/Components/FillInTheBlanksBox.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export default function FillInTheBlanksBox (props) {
+export default function FillInTheBlanksBox(props) {
     return (
-        <div 
-            className="text-center bg-white p-1 shadow sm:p-4 border border-black border-3 max-w-1/4 cursor-pointer border-1 rounded-lg" 
-            onClick={ () => props.remove(props.showText) }
+        <div
+            className="text-center bg-white p-1 shadow p-1 sm:p-2 border border-black border-3 min-w-[45px] max-w-1/4 cursor-pointer border-1 rounded-sm"
+            onClick={() => props.remove(props.showText)}
         >
-            { props.showText }
+            {props.showText}
         </div>
-    )
+    );
 }
