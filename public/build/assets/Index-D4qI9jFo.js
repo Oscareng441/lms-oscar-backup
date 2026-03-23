06d95f2702b6e5051d3cd858f43453e09308aea5
jfarmer <jfarmer@feisrt.com>
2026-03-23 11:15:47 -0600

    want list style for ol

--- public/build/assets/Index-D4qI9jFo.js
+++
@@ -1,1 +0,0 @@
-import{j as r,B as s}from"./app-BBMWSGCK.js";import{A as o}from"./AuthenticatedLayout-BxTmW74a.js";import"./ApplicationLogo-DZ9EVpVU.js";import"./transition-DjsCANSj.js";const p=({auth:t,data:a})=>{const e="Problems";return r.jsx(o,{auth:t,user:t.user,header:e,children:r.jsx(s,{title:e})})};export{p as default};
---
+++ public/build/assets/index-By-Rldny.js
@@ -0,0 +1,1 @@
+import{b as r}from"./app-C5an5XP_.js";var a=r();export{a as r};
